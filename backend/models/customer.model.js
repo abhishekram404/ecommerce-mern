bdf1c917da1db237662ecf132effd2f33bd48e72
Keshav Kishor Ram <ramabishek40@gmail.com>
2021-12-22 09:30:11 +0545

    Add alerts while creating products.

--- backend/models/customer.model.js
+++ backend/models/customer.model.js
@@ -6,6 +6,7 @@ const customerSchema = mongoose.Schema({
   password: { type: String, select: false },
   products: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Customer" }],
   orders: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Order" }],
+  cart: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Product" }],
 });
 
 const Customer = mongoose.model("Customer", customerSchema);
--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 import {
+  ERROR,
   GET_ALL_PRODUCTS_FAIL,
   GET_ALL_PRODUCTS_SUCCESS,
   PRODUCT_CREATE_FAIL,
   PRODUCT_CREATE_SUCCESS,
+  SUCCESS,
 } from "redux/constants";
 
 export const getAllProducts = () => {
@@ -15,13 +17,21 @@ export const getAllProducts = () => {
           type: GET_ALL_PRODUCTS_SUCCESS,
           payload: data.details,
         });
-        dispatch({
-          type: GET_ALL_PRODUCTS_FAIL,
-        });
       }
+      dispatch({
+        type: ERROR,
+        payload: data.message,
+      });
+      dispatch({
+        type: GET_ALL_PRODUCTS_FAIL,
+      });
     } catch (error) {
       dispatch({
         type: GET_ALL_PRODUCTS_FAIL,
+      });
+      dispatch({
+        type: ERROR,
+        payload: "Something went wrong while fetching the products.",
       });
     }
   };
@@ -33,8 +43,11 @@ export const sendProductAddRequest = (product) => {
       const { data } = await axios.post("/product/create", product, {
         withCredentials: true,
       });
-      console.log(data);
       if (data.success) {
+        dispatch({
+          type: SUCCESS,
+          payload: data.message,
+        });
         return dispatch({
           type: PRODUCT_CREATE_SUCCESS,
           // payload: data.detais,
@@ -43,8 +56,15 @@ export const sendProductAddRequest = (product) => {
       dispatch({
         type: PRODUCT_CREATE_FAIL,
       });
+      dispatch({
+        type: ERROR,
+        payload: data.message,
+      });
     } catch (error) {
-      console.log(error.message);
+      dispatch({
+        type: ERROR,
+        payload: "Something went wrong while creating new product.",
+      });
       dispatch({
         type: PRODUCT_CREATE_FAIL,
       });
